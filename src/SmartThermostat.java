a12fc74ca187edfc160481682c94f205cb0b51ae
ManuelaAriasG <manuelaarias11@gmail.com>
2025-04-04 17:45:06 -0500

    terminada clase SmartThermostat

--- src/SmartThermostat.java
+++ src/SmartThermostat.java
@@ -1,15 +1,42 @@
 public class SmartThermostat extends SmartDevice {
-   private double temperature;
+    private double temperature;
 
-   public SmartThermostat(String brand, String model, String status, double temperature){
-       super (brand,model,status);
-       this.temperature=temperature;
-   }
+    public SmartThermostat(String brand, String model, String status, double temperature) {
+        super(brand, model, status);
+        this.temperature = temperature;
 
+    }
 
+    public double getTemperature() {
+        return temperature;
+    }
 
+    public void setTemperature(double temperature) {
+        if (temperature<10 || temperature>30){
+            System.out.println("La temperatura está fuera del rango, debe ser mayor de 10° y menor de 30°");
+        }
+        else {
 
-public void deviceInfo(){
-       System.out.println("SmartThermostat - Brand: )" + brand + ", Model: " + model + ", Status: " + status);
-}
-}
+            this.temperature = temperature;
+        }
+    }
+
+    public void increaseTemperature(){
+        temperature +=1;
+    }
+
+    public void decreaseTemperature(){
+        temperature -=1;
+    }
+
+    public void isEnergySavingMode(){
+        if (temperature>10 && temperature <20){
+            System.out.println("El termostato se encuentra en modo ahorro de energía");
+        }
+    }
+
+        public void deviceInfo() {
+            System.out.println("SmartThermostat - Brand: " + brand + ", Model: " + model + ", Status: " + status);
+        }
+    }
+
