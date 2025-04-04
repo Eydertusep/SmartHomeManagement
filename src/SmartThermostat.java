6e3356f5bf38e8c0d13325ada8e2ca6dd733fbe5
ManuelaAriasG <manuelaarias11@gmail.com>
2025-04-04 07:22:32 -0500

    atributos del termostato y metodo del padre

--- src/SmartThermostat.java
+++ src/SmartThermostat.java
@@ -1,2 +1,15 @@
-public class SmartThermostat {
+public class SmartThermostat extends SmartDevice {
+   private double temperature;
+
+   public SmartThermostat(String brand, String model, String status, double temperature){
+       super (brand,model,status);
+       this.temperature=temperature;
+   }
+
+
+
+
+public void deviceInfo(){
+       System.out.println("SmartThermostat - Brand: )" + brand + ", Model: " + model + ", Status: " + status);
+}
 }
