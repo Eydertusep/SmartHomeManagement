0bd76f172607b631efe46bb50de502aa613714a3
[Eyder] <[eyder.alexander.riwi@gmail.com]>
2025-04-04 17:22:54 -0500

    Termine la clse SmartLight y trabaje en el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        SmartLight hue = new SmartLight("Philips", "Hue", "ON", "white", 30);
+        SmartThermostat nest = new SmartThermostat("Nest", "T3007ES", "ON", 22);
+        hue.turnOn();
+        nest.turnOn();
+
+        hue.deviceInfo();
+        hue.setAdjustBrightness(75);
+        hue.getJustBrightness();
+
+        nest.deviceInfo();
+        nest.
+
+
+
+
     }
 }
--- src/SmartLight.java
+++ src/SmartLight.java
@@ -1,2 +1,36 @@
-public class SmartLight {
-}
+public class SmartLight extends SmartDevice {
+
+    private int brightness;
+    private String color;
+
+    public SmartLight(String brand, String model, String status, String color, int level) {
+        super(brand, model, status);
+        this.color = color;
+
+    }
+
+
+    public void changeColor(String newColor) {
+        this.color = newColor;
+    }
+
+    public void setAdjustBrightness(int level) {
+        if (level >= 0 && level < 101) {
+            this.brightness = level;
+        } else {
+            if (level < 0) {
+                System.out.println(" el brillo es negativo ");
+            } else {
+                System.out.println(" El brillo supera el 100% ");
+            }
+        }
+    }
+
+    public void getJustBrightness() {
+        System.out.println(" Brightness: " + brightness);
+    }
+
+    public void deviceInfo () {
+        System.out.println("Brand: " + brand + " Model: " + model + " Status: " + status + " Color: " + color);
+        };
+    }
